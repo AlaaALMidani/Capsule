3927b8b0f63d568bebac933c6a9c89a9c94e20d0
Alaa Al-Midani <alaaalmedane@gmail.com>
2024-11-15 22:19:33 +0300

    merging with kamel

---
+++ frontend/src/services/userServices.js
@@ -0,0 +1,37 @@
+const baseUrl = "http://localhost:3002/api/users/";
+const register = "register";
+const login = "login"
+export class UserServices {
+
+    static async register(data) {
+        console.log('ri')
+        console.log(data)
+        return fetch(`${baseUrl}${register}`, {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then(data => data)
+            .catch(error =>
+                error.message
+            );
+    }
+
+    static async login(data) {
+        return fetch(`${baseUrl}${login}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then(data => data)
+            .catch(error =>
+                error.message
+            );
+    }
+}
